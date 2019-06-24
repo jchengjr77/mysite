09fc24ba2058f6da8606500929128f1d68f8d4b9
jchengjr77 <jcheng3@andrew.cmu.edu>
2019-06-24 10:34:51 +0800

    Updates

---
+++ precache-manifest.0798b7c7806fcff5f232fed91b547ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b0e0f5c969ab980cb404a6c8979b57",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "14063d31acb53597fb55",
+    "url": "/mysite/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "26179b7f6f5958da50d7",
+    "url": "/mysite/static/css/main.c22aba85.chunk.css"
+  },
+  {
+    "revision": "14063d31acb53597fb55",
+    "url": "/mysite/static/js/2.4a5e165e.chunk.js"
+  },
+  {
+    "revision": "26179b7f6f5958da50d7",
+    "url": "/mysite/static/js/main.9254451c.chunk.js"
+  },
+  {
+    "revision": "69f460fd7b0f3581d72a",
+    "url": "/mysite/static/js/runtime~main.2f907dfc.js"
+  },
+  {
+    "revision": "458eee97649c115a63ad8dbe5b4c150f",
+    "url": "/mysite/static/media/profpic.458eee97.jpg"
+  }
+]);
