22113b8e8058bf1033999b4b1715f752e0a4d743
jchengjr77 <jcheng3@andrew.cmu.edu>
2019-07-27 16:16:48 +0800

    Able to save input on the form. Next step, send info to personal email.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,27 +1,57 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
-import { isLVal } from "@babel/types";
+import { Form, FormGroup, Label, Input } from "reactstrap";
 
 export class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    console.log("Form submitted");
+    const name = document.getElementById("personName").value;
+    const email = document.getElementById("personEmail").value;
+    const message = document.getElementById("personBody").value;
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Message: " + message);
+  }
+
   render() {
     return (
       <div className="contact-cont" id="contact">
-        <a className="contact-anchor" id="contact"></a>
+        <a className="contact-anchor" id="contact" />
         <h1 className="subheader">Contact:</h1>
-        <Form>
+        <Form onSubmit={() => this.handleSubmit()}>
           <FormGroup>
             <Label for="personName">Name: (what should I call you?)</Label>
-            <Input id="personName" placeholder="John Doe" required/>
+            <Input
+              name="name"
+              id="personName"
+              placeholder="John Doe"
+              required
+            />
           </FormGroup>
           <FormGroup>
             <Label for="personEmail">Email: (where should I reply to?)</Label>
-            <Input id="personEmail" placeholder="johndoe@gmail.com" required/>
+            <Input
+              name="email"
+              id="personEmail"
+              placeholder="johndoe@gmail.com"
+              required
+            />
           </FormGroup>
           <FormGroup>
             <Label for="message">Message: (what's up?)</Label>
-            <Input type="textarea" id="exampleText" placeholder="I was wondering about your..." required/>
+            <Input
+              name="message"
+              type="textarea"
+              id="personBody"
+              placeholder="I was wondering about your..."
+              required
+            />
           </FormGroup>
-          <Input type="submit" id="submit-button" value="Send Message"></Input>
+          <Input type="submit" id="submit-button" value="Send Message" />
         </Form>
       </div>
     );
